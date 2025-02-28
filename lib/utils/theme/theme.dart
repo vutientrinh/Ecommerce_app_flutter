7df8f0ca880ba6abab684b6025cb163a81b4e2c6
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2025-02-28 15:35:51 +0700

    Custom theme

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,3 +1,10 @@
+import 'package:ecommerce_app_flutter/utils/theme/custom_themes/appbar_theme.dart';
+import 'package:ecommerce_app_flutter/utils/theme/custom_themes/bottom_sheet_theme.dart';
+import 'package:ecommerce_app_flutter/utils/theme/custom_themes/checkbox_theme.dart';
+import 'package:ecommerce_app_flutter/utils/theme/custom_themes/chip_theme.dart';
+import 'package:ecommerce_app_flutter/utils/theme/custom_themes/elevated_button_theme.dart';
+import 'package:ecommerce_app_flutter/utils/theme/custom_themes/outlined_button_theme.dart';
+import 'package:ecommerce_app_flutter/utils/theme/custom_themes/text_field_theme.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_themes/text_theme.dart';
@@ -10,14 +17,28 @@ class TAppTheme{
     brightness: Brightness.light,
     primaryColor: Colors.blue,
     scaffoldBackgroundColor: Colors.white,
-    textTheme: TTextTheme.lightTextTheme
+    textTheme: TTextTheme.lightTextTheme,
+    chipTheme: TChipTheme.lightChipTheme,
+    appBarTheme: TAppBarTheme.lightAppBarTheme,
+    checkboxTheme: TCheckboxTheme.lightCheckboxTheme,
+    bottomSheetTheme: TBottomSheetTheme.lightBottomSheetTheme,
+    elevatedButtonTheme: TElevatedButtonTheme.lightElevatedButtonTheme,
+    outlinedButtonTheme: TOutlinedButtonTheme.lightOutlinedButtonTheme,
+    inputDecorationTheme: TTextFormFieldTheme.lightInputDecorationTheme,
   );
   static ThemeData darkTheme = ThemeData(
-      useMaterial3: true,
-      fontFamily: 'Poppins',
-      brightness: Brightness.dark,
-      primaryColor: Colors.blue,
-      scaffoldBackgroundColor: Colors.black,
-      textTheme: TTextTheme.darkTextTheme
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: TTextTheme.darkTextTheme,
+    chipTheme: TChipTheme.darkChipTheme,
+    appBarTheme: TAppBarTheme.darkAppBarTheme,
+    checkboxTheme: TCheckboxTheme.darkCheckboxTheme,
+    bottomSheetTheme: TBottomSheetTheme.darkBottomSheetTheme,
+    elevatedButtonTheme: TElevatedButtonTheme.darkElevatedButtonTheme,
+    outlinedButtonTheme: TOutlinedButtonTheme.darkOutlinedButtonTheme,
+    inputDecorationTheme: TTextFormFieldTheme.darkInputDecorationTheme,
   );
 }
