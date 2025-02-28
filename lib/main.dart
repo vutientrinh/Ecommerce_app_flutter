8b79e18ba82ec8343d36d75bf2558a159589b614
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2025-02-28 17:19:12 +0700

    Color custom - colors.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app_flutter/utils/app.dart';
 import 'package:ecommerce_app_flutter/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 
@@ -5,16 +6,3 @@ void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return  MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-    );
-
-  }
-}
